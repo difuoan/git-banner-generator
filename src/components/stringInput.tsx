6075e42ca855a817777afaeebe8349a09af8f1af
Lucas Venturini <lucas.j.venturini@gmail.com>
2024-06-02 22:16:24 +0200

    added string input

---
+++ src/components/stringInput.tsx
@@ -0,0 +1,28 @@
+const StringInput = ({
+  onChange,
+  value,
+  label,
+}: {
+  onChange: Function;
+  value: string;
+  label: string;
+}) => {
+  return (
+    <div className={"flex flex-row gap-8 items-center"}>
+      <label
+        htmlFor={label}
+        className="block mb-2 font-medium text-gray-900 dark:text-white"
+      >
+        {label}
+      </label>
+      <input
+        type="text"
+        id={label}
+        value={value}
+        onChange={(event) => onChange(event.target.value)}
+        className="appearance-none block w-full bg-gray-300 text-gray-700 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-400"
+      />
+    </div>
+  );
+};
+export default StringInput;
