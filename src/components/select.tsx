067aca0c45f618eb5a3f150c9382a3793624ce7c
Lucas Venturini <lucas.j.venturini@gmail.com>
2024-06-02 23:05:51 +0200

    added select component

---
+++ src/components/select.tsx
@@ -0,0 +1,39 @@
+const Select = ({
+  onChange,
+  value,
+  label,
+  options,
+}: {
+  onChange: Function;
+  value?: string;
+  label: string;
+  options: string[];
+}) => {
+  const optionsHtml = options.map((option: string, index: number) => (
+    <option key={index} value={option}>
+      {option}
+    </option>
+  ));
+  return (
+    <div className={"flex flex-row gap-8 items-center"}>
+      <label
+        htmlFor={label}
+        className="block mb-2 font-medium text-gray-900 dark:text-white"
+      >
+        {label}
+      </label>
+      <select
+        id={label}
+        value={value ?? "none"}
+        onChange={(event) => onChange(event.target.value)}
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      >
+        <option key="none" value={"none"}>
+          none
+        </option>
+        {optionsHtml}
+      </select>
+    </div>
+  );
+};
+export default Select;
