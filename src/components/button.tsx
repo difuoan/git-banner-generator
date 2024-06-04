a1b99ec5dc2944b392929d2e1dad9549caffaad6
Lucas Venturini <lucas.j.venturini@gmail.com>
2024-06-05 01:52:14 +0200

    imporved the button component and made it disable-able

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,31 +1,32 @@
-import { MouseEventHandler } from "react";
+import { ButtonHTMLAttributes, MouseEventHandler } from "react";
 
 const Button = ({
   onClick,
   label,
   color = "blue",
+  disabled,
   colorStrength = 5,
-}: {
+}: Partial<
+  JSX.IntrinsicAttributes & ButtonHTMLAttributes<HTMLButtonElement>
+> & {
   onClick: MouseEventHandler<HTMLButtonElement>;
   label: string;
   color?: string;
   colorStrength?: number;
 }) => {
+  const classesToUse = disabled
+    ? `bg-${color}-${
+        colorStrength * 100 - 200
+      } cursor-not-allowed text-white font-bold py-2 px-4 rounded`
+    : `bg-${color}-${colorStrength * 100} hover:bg-${color}-${
+        colorStrength * 100 + 200
+      } text-white font-bold py-2 px-4 rounded`;
   return (
     <button
+      disabled={disabled}
       type="button"
       onClick={onClick}
-      className={
-        `bg-` +
-        color +
-        `-` +
-        colorStrength * 100 +
-        ` hover:bg-` +
-        color +
-        `-` +
-        (colorStrength * 100 + 200) +
-        ` text-white font-bold py-2 px-4 rounded`
-      }
+      className={classesToUse}
     >
       {label}
     </button>
