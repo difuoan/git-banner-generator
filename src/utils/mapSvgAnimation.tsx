f434196f0e68e1a3660806310b6470c3b5320c46
Lucas Venturini <lucas.j.venturini@gmail.com>
2024-06-09 20:39:16 +0200

    be more confident

--- src/utils/mapSvgAnimation.tsx
+++ src/utils/mapSvgAnimation.tsx
@@ -5,15 +5,15 @@ export const mapSvgAnimation = (animation: SvgAnimation, index: number) => {
   return (
     <animate
       key={index}
-      attributeName={animation?.attributeName}
-      from={animation?.from}
-      to={animation?.to}
-      dur={animation?.dur + "s"}
-      begin={animation?.begin + "s"}
-      repeatCount={animation?.repeatCount}
+      attributeName={animation.attributeName}
+      from={animation.from}
+      to={animation.to}
+      dur={animation.dur + "s"}
+      begin={animation.begin + "s"}
+      repeatCount={animation.repeatCount}
       calcMode="spline"
       keyTimes="0; 1"
-      keySplines={splines[animation?.keySplines]}
+      keySplines={splines[animation.keySplines]}
     />
   );
 };
