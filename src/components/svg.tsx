16e07a573705a2a9bd36bb1758afa9a3bd2adc92
Lucas Venturini <lucas.j.venturini@gmail.com>
2024-06-12 20:03:33 +0200

    only include each typeface once

--- src/components/svg.tsx
+++ src/components/svg.tsx
@@ -1,7 +1,8 @@
 import { SvgElement } from "@/types/svgElement";
+import { isSvgTextElement } from "@/types/svgTextElement";
 import { mapSvgDefinitions } from "@/utils/elements/mapSvgDefinitions";
 import { mapSvgElement } from "@/utils/elements/mapSvgElement";
-import { JSX, SVGProps, useEffect, useState } from "react";
+import { JSX, SVGProps } from "react";
 
 const SVGComponent = (
   props: Partial<JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>> & {
@@ -11,7 +12,15 @@ const SVGComponent = (
     svgbackground: string;
   }
 ) => {
-  const definitions = props.elements.map(mapSvgDefinitions);
+  const uniqueDefs: string[] = [];
+  const definitions = props.elements
+    .filter(isSvgTextElement)
+    .filter((ele) => {
+      if (uniqueDefs.includes(ele.fontFamily)) return false;
+      uniqueDefs.push(ele.fontFamily);
+      return true;
+    })
+    .map(mapSvgDefinitions);
   const elementHtml = props.elements.map(mapSvgElement);
   return (
     <svg
